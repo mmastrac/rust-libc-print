691d159ff8fafdbc4ff2e37636025390c5519da3
Matt Mastracci <matthew@mastracci.com>
2026-05-04 08:12:39 -0600

    windows?

--- examples/writeln.rs
+++ examples/writeln.rs
@@ -1,8 +1,14 @@
 #![cfg_attr(nostartfiles, no_main)]
 use libc_print::*;
 
-#[cfg(nostartfiles)]
+#[cfg(all(nostartfiles, not(windows)))]
 fn _start() {
+    libc_println!("Hello, {}!", "world");
+}
+
+#[cfg(all(nostartfiles, windows))]
+#[no_mangle]
+fn mainCRTStartup() {
     libc_println!("Hello, {}!", "world");
 }
 
