59fff12e3cdb41cd76a2ed0f4b117c2bc885e4bc
Matt Mastracci <matthew@mastracci.com>
2026-05-04 08:08:17 -0600

    nostartfiles

--- examples/writeln.rs
+++ examples/writeln.rs
@@ -1,5 +1,12 @@
+#![cfg_attr(nostartfiles, no_main)]
 use libc_print::*;
 
+#[cfg(nostartfiles)]
 fn _start() {
+    libc_println!("Hello, {}!", "world");
+}
+
+#[cfg(not(nostartfiles))]
+fn main() {
     libc_println!("Hello, {}!", "world");
 }
